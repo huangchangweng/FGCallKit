57adf76024e66dd9c49f2ad99569afdca04257e7
huangchangweng <599139419@qq.com>
2023-04-18 15:29:12 +0800

    0.8.4

--- Example/Pods/Target Support Files/FGCallKit/FGCallKit-umbrella.h
+++ Example/Pods/Target Support Files/FGCallKit/FGCallKit-umbrella.h
@@ -10,33 +10,8 @@
 #endif
 #endif
 
-#import "FGAccountInfoModel.h"
-#import "FGCall.h"
 #import "FGCallKit.h"
-#import "FGCompanyModel.h"
-#import "FGHttpManager.h"
-#import "FGKitInfo.h"
-#import "FGUserModel.h"
-#import "FGUtils.h"
-#import "Gossip.h"
-#import "GSAccount+Private.h"
-#import "GSAccount.h"
-#import "GSAccountConfiguration.h"
-#import "GSCall+Private.h"
-#import "GSCall.h"
-#import "GSCodecInfo+Private.h"
-#import "GSCodecInfo.h"
-#import "GSConfiguration.h"
-#import "GSDispatch.h"
-#import "GSIncomingCall.h"
-#import "GSNotifications.h"
-#import "GSOutgoingCall.h"
-#import "GSPJUtil.h"
-#import "GSRingback.h"
-#import "GSUserAgent+Private.h"
-#import "GSUserAgent.h"
-#import "PJSIP.h"
-#import "Util.h"
+#import "FGCall.h"
 
 FOUNDATION_EXPORT double FGCallKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char FGCallKitVersionString[];
