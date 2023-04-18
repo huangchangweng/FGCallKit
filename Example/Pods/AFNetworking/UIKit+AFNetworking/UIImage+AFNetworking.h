72b347d3d8dd4cafa33bc4eb3a0b0e16351bfcbc
huangchangweng <599139419@qq.com>
2023-04-18 17:30:03 +0800

    0.8.7

--- Example/Pods/AFNetworking/UIKit+AFNetworking/UIImage+AFNetworking.h
+++ Example/Pods/AFNetworking/UIKit+AFNetworking/UIImage+AFNetworking.h
@@ -1,6 +1,8 @@
-// UIKit+AFNetworking.h
 //
-// Copyright (c) 2011–2016 Alamofire Software Foundation ( http://alamofire.org/ )
+//  UIImage+AFNetworking.h
+//  
+//
+//  Created by Paulo Ferreira on 08/07/15.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,22 +23,13 @@
 // THE SOFTWARE.
 
 #if TARGET_OS_IOS || TARGET_OS_TV
+
 #import <UIKit/UIKit.h>
 
-#ifndef _UIKIT_AFNETWORKING_
-    #define _UIKIT_AFNETWORKING_
+@interface UIImage (AFNetworking)
 
-#if TARGET_OS_IOS
-    #import "AFAutoPurgingImageCache.h"
-    #import "AFImageDownloader.h"
-    #import "AFNetworkActivityIndicatorManager.h"
-    #import "UIRefreshControl+AFNetworking.h"
-    #import "WKWebView+AFNetworking.h"
-#endif
++ (UIImage *)safeImageWithData:(NSData *)data;
 
-    #import "UIActivityIndicatorView+AFNetworking.h"
-    #import "UIButton+AFNetworking.h"
-    #import "UIImageView+AFNetworking.h"
-    #import "UIProgressView+AFNetworking.h"
-#endif /* _UIKIT_AFNETWORKING_ */
+@end
+
 #endif
